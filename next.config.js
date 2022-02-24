aa06389bd4769018e213dc76a2f27206f0413a72
Alisson Patricio <eu@alisson.net>
2022-02-24 10:35:36 -0300

    change page extension to .page.tsx
    this will allow us to have components inside the pages folder

--- next.config.js
+++ next.config.js
@@ -4,4 +4,5 @@ const withTM = require('next-transpile-modules')(['baseapp-nextjs-core']);
 
 module.exports = withTM({
   reactStrictMode: true,
+  pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
 })

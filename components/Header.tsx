0170f5676979317616ce8aea1bc17b6af55b4547
Alisson Patricio <eu@alisson.net>
2022-02-24 11:04:10 -0300

    make header a little prettier

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,26 +1,31 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import { useUser } from 'baseapp-nextjs-core'
+import { AppBar, Toolbar, Button, Typography, Link as MuiLink } from '@mui/material'
 
 function Header() {
   const { user } = useUser()
 
-  return <header>
-    <h1><Image src="/logo.png" alt="BaseApp Logo" width={40} height={40} /> BaseApp</h1>
+  return <AppBar position="static" component="header" sx={{ marginBottom: 2 }}>
+    <Toolbar>
+      <Link href="/"><Button><Image src="/logo.png" alt="BaseApp Logo" width={40} height={40} /></Button></Link>
 
-    <nav>
-      <Link href="/">Home</Link>
+      <Typography variant="h1" component="h1" sx={{ flexGrow: 1, marginLeft: 1 }}>
+        <Link href="/" passHref><MuiLink underline="hover" color="inherit">BaseApp</MuiLink></Link>
+      </Typography>
+
+      <Link href="/" passHref><Button color="inherit">Home</Button></Link>
       {!user && <>
-        <Link href="/auth/login">Login</Link>
-        <Link href="/auth/signup">Sign Up</Link>
+        <Link href="/auth/login" passHref><Button color="inherit">Login</Button></Link>
+        <Link href="/auth/signup" passHref><Button color="inherit">Sign Up</Button></Link>
       </>}
 
       {user && <>
         <span>Welcome {user.email}</span>
-        <Link href="/auth/logout">Logout</Link>
+        <Link href="/auth/logout"><Button color="inherit">Logout</Button></Link>
       </>}
-    </nav>
-  </header>
+    </Toolbar>
+  </AppBar>
 }
 
 export default Header

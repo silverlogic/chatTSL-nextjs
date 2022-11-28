3872e026a61918d75f060464bc273ac455fc5bd0
Filipe Oliveira <fc@tsl.io>
2022-11-28 11:33:13 +0000

    Merged in BA-Add-OG-Metatags (pull request #17)
    BA-Add-OG-Metatags
    
    * BA-Add-OG-Metatags
    
    
    Approved-by: Gabriel Santos
    Approved-by: Alexandre Anicio
    Approved-by: Lucas Procopio

--- app-templates/base-project/pages/_app.page.tsx
+++ app-templates/base-project/pages/_app.page.tsx
@@ -36,12 +36,42 @@ const App = (props: AppProps) => {
       <SnackbarProvider maxSnack={3} autoHideDuration={5000}>
         <CacheProvider value={emotionCache}>
           <Head>
-            <title>BaseApp</title>
+            <title>TSL BaseApp</title>
             <meta
               name="viewport"
               content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, shrink-to-fit=no, user-scalable=no"
             />
             <link rel="icon" href="/favicon.ico" />
+            <meta name="robots" content="max-image-preview:large" />
+            {/* <!-- TODO: Replace "content" with your own --> */}
+            {/* <!-- MS, fb & Whatsapp --> */}
+
+            {/* <!-- MS Tile - for Microsoft apps--> */}
+            <meta name="msapplication-TileImage" content="https://mar-api-production.s3.amazonaws.com/media/skills/7266c5a1-1aaa-4939-a17e-66a0c6fb2358.png" />
+
+            {/* <!-- fb & Whatsapp --> */}
+
+            {/* <!-- Site Name, Title, and Description to be displayed --> */}
+            <meta property="og:site_name" content="TSL BaseApp" />
+            <meta property="og:title" content="TSL BaseApp" />
+            <meta
+              property="og:description"
+              content="A NextJS baseapp for TSL projects"
+            />
+
+            {/* <!-- Image to display --> */}
+            <meta property="og:image" content="https://mar-api-production.s3.amazonaws.com/media/skills/7266c5a1-1aaa-4939-a17e-66a0c6fb2358.png" />
+
+            {/* <!-- No need to change anything here --> */}
+            <meta property="og:type" content="website" />
+            <meta property="og:image:type" content="image/png" />
+
+            {/* <!-- Size of image. Any size up to 300. Anything above 300 will not work in WhatsApp --> */}
+            <meta property="og:image:width" content="300" />
+            <meta property="og:image:height" content="300" />
+
+            {/* <!-- Website to visit when clicked in fb or WhatsApp--> */}
+            <meta property="og:url" content="https://tsl.io/"></meta>
           </Head>
           <ThemeProvider theme={theme}>
             {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
